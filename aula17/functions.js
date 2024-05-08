f79374ed8d443de1886fb35630452ac45bef607e
Luan Costa PC <luan-esposito@hotmail.com>
2024-05-08 00:57:28 -0300

    functions

---
+++ aula17/functions.js
@@ -0,0 +1,25 @@
+const raiz = n => n ** 0.5; //cria função com arrow function
+
+console.log(raiz(9))
+
+// const raiz = function(n){ //criar função diretamente na variavel
+//     return n**0.5;
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+// function soma(x = 10, y = 1){ //caso não seja atribuido nenhujm valor para os parametros na execução da função, ele assume os valores declarados.
+//     const resultado = x+y;
+//     return resultado;
+// }
+
+// console.log(soma(1800));
+
+// function saudacao(nome){
+//     return `Bom dia! ${nome}`;
+// }
+
+// const variavel = saudacao('Luan');
+// console.log(variavel);
